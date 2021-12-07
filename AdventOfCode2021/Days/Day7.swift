036c27150b9fb469d6442fb34d45cfc0e18f7aab
Bastian Stien <bastian.stien@gmail.com>
2021-12-07 18:08:40 +0100

    Day 7 – part 2

--- AdventOfCode2021/Days/Day7.swift
+++ AdventOfCode2021/Days/Day7.swift
@@ -3,18 +3,20 @@ import Foundation
 struct Day7: Day {
     static func run(input: String) {
         let positions = splitInput(input).first!.split(separator: ",").map(String.init).compactMap(Int.init)
-        part1(positions: positions)
+        findOptimalPosition(dayPart: 1, positions: positions, distanceMultiplier: { $0 })
+        findOptimalPosition(dayPart: 2, positions: positions, distanceMultiplier: { ($0 * ($0 + 1)) / 2 })
     }
 
-    private static func part1(positions: [Int]) {
-        let minValue = positions.min()!
-        let maxValue = positions.max()!
+    private static func findOptimalPosition(dayPart: Int, positions: [Int], distanceMultiplier: (Int) -> Int) {
+        var positionsWithCount = Array(repeating: 0, count: positions.max()! + 1)
+        positions.forEach { positionsWithCount[$0] += 1 }
+        let positionsWithCrabs = Array(Set(positions))
 
-        let optimalPosition = (minValue..<maxValue).map { value -> (Int, Int) in
-            let fuel = positions.map { abs($0 - value) }.reduce(0, +)
-            return (value, fuel)
+        let optimalPosition = (0..<positionsWithCount.count).map { index -> (Int, Int) in
+            let fuel = positionsWithCrabs.map { distanceMultiplier(abs($0 - index)) * positionsWithCount[$0] }.reduce(0, +)
+            return (index, fuel)
         }.min(by: { $0.1 <= $1.1 })!
 
-        printResult(dayPart: 1, message: "Position \(optimalPosition.0), fuel \(optimalPosition.1)")
+        printResult(dayPart: dayPart, message: "Position \(optimalPosition.0), fuel \(optimalPosition.1)")
     }
 }
