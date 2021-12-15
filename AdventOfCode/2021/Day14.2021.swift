cedd8b175e06af35be84f548b6515d5357ec10fa
Bastian Stien <bastian.stien@gmail.com>
2021-12-15 15:29:40 +0100

    Day 14 using a recursive method – part 2 still not accessible

--- AdventOfCode/2021/Day14.2021.swift
+++ AdventOfCode/2021/Day14.2021.swift
@@ -4,43 +4,50 @@ import Algorithms
 extension Year2021.Day14: Runnable {
     func run(input: String) {
         let (polymer, rules) = parse(input: input)
-        let part1 = iterate(iterations: 10, polymer: polymer, rules: rules)
 
+        let part1 = start(iterations: 10, polymer: polymer, rules: rules)
         printResult(dayPart: 1, message: "Sum after 10 iterations: \(part1)")
+
+        let part2 = start(iterations: 40, polymer: polymer, rules: rules)
+        printResult(dayPart: 2, message: "Sum after 40 iterations: \(part2)")
     }
 
-    private func iterate(iterations: Int, polymer: String, rules: [String: String]) -> Int {
-        var polymer = polymer
-
-        for _ in (0..<iterations) {
-            var inserted = 0
-            var newPolymer = polymer
-            for (index, window) in polymer.windows(ofCount: 2).enumerated() {
-                if let toInsert = rules[String(window)] {
-                    newPolymer.insert(contentsOf: toInsert, at: newPolymer.index(newPolymer.startIndex, offsetBy: index + inserted + 1))
-                    inserted += 1
-                }
-            }
-            polymer = newPolymer
-        }
-
-        let grouped = polymer.reduce(into: [Character: Int]()) { result, character in
+    func start(iterations: Int, polymer: String, rules: [String: Character]) -> Int {
+        var countDict = polymer.reduce(into: [Character: Int]()) { result, character in
             result[character, default: 0] += 1
         }
 
-        let min = grouped.map(\.value).min()!
-        let max = grouped.map(\.value).max()!
+        polymer.windows(ofCount: 2).forEach { pair in
+            iterate(iterations: iterations, characters: String(pair), rules: rules, countDict: &countDict)
+        }
+
+        let min = countDict.map(\.value).min()!
+        let max = countDict.map(\.value).max()!
         return max - min
     }
 
-    private func parse(input: String) -> (String, [String: String]) {
+    func iterate(iterations: Int, characters: String, rules: [String: Character], countDict: inout [Character: Int]) {
+        guard
+            iterations != 0,
+            let characterToAdd = rules[characters]
+        else { return }
+
+        countDict[characterToAdd, default: 0] += 1
+
+        let pairA = "\(characters.first!)\(characterToAdd)"
+        let pairB = "\(characterToAdd)\(characters.last!)"
+        iterate(iterations: iterations - 1, characters: pairA, rules: rules, countDict: &countDict)
+        iterate(iterations: iterations - 1, characters: pairB, rules: rules, countDict: &countDict)
+    }
+
+    private func parse(input: String) -> (String, [String: Character]) {
         var lines = splitInput(input)
         let polymer = lines.removeFirst()
 
-        var rules = [String: String]()
+        var rules = [String: Character]()
         for line in lines {
             let split = splitInput(line, separator: " ")
-            rules[split[0]] = split[2]
+            rules[split[0]] = split[2].first!
         }
 
         return (polymer, rules)
