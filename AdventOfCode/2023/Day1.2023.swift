880f020233d37fefc5995ee176031238279d8f1b
Bastian Stien <bastian.stien@gmail.com>
2023-12-01 15:22:42 +0100

    Day 1 optimization

--- AdventOfCode/2023/Day1.2023.swift
+++ AdventOfCode/2023/Day1.2023.swift
@@ -19,52 +19,49 @@ extension Year2023.Day1: Runnable {
 
     private func findTotal(from lines: [String], includeSpelledNumbers: Bool) -> Int {
         lines.compactMap { line in
-            let occurrences = numberMapping.flatMap { mapping in
-                var occurrences = [findOccurrences(of: String(mapping.0), in: line, intValue: mapping.0)]
-
-                if includeSpelledNumbers {
-                    occurrences += [findOccurrences(of: mapping.1, in: line, intValue: mapping.0)]
-                }
-                return occurrences
-            }.compactMap { $0 }
+            let firstOccurences = findOccurences(in: line, includeSpelledNumbers: includeSpelledNumbers)
+            let lastOccurences = findOccurences(in: line, includeSpelledNumbers: includeSpelledNumbers, compareOption: .backwards)
 
             guard
-                let first = occurrences.min(by: { $0.first < $1.first }),
-                let last = occurrences.max(by: { $0.last < $1.last })
+                let first = firstOccurences.min(by: { $0.position < $1.position }),
+                let last = lastOccurences.max(by: { $0.position < $1.position })
             else { return nil }
 
             return (first.intValue * 10) + last.intValue
         }.reduce(0, +)
     }
 
-    private func findOccurrences(of string: String, in line: String, intValue: Int) -> NumberOccurrence? {
-        let ranges = line.ranges(of: string).compactMap { $0 }
+    private func findOccurences(in line: String, includeSpelledNumbers: Bool, compareOption: String.CompareOptions = []) -> [NumberOccurrence] {
+        func findOccurrence(of string: String, intValue: Int) -> NumberOccurrence? {
+            guard let range = line.range(of: string, options: compareOption) else { return nil }
+            return NumberOccurrence(intValue: intValue, position: range.lowerBound.utf16Offset(in: line))
+        }
 
-        guard
-            let first = ranges.compactMap({ $0.lowerBound.encodedOffset }).min(),
-            let last = ranges.compactMap({ $0.upperBound.encodedOffset }).max()
-        else { return nil }
-
-        return NumberOccurrence(intValue: intValue, first: first, last: last)
+        return numberMapping.flatMap { mapping in
+            [
+                findOccurrence(of: String(mapping.intValue), intValue: mapping.intValue),
+                includeSpelledNumbers ? findOccurrence(of: mapping.spelled, intValue: mapping.intValue) : nil
+            ]
+        }.compactMap { $0 }
     }
 
-    private var numberMapping: [(Int, String)] {
+
+    private var numberMapping: [(intValue: Int, spelled: String)] {
         [
-            (1, "one"),
-            (2, "two"),
-            (3, "three"),
-            (4, "four"),
-            (5, "five"),
-            (6, "six"),
-            (7, "seven"),
-            (8, "eight"),
-            (9, "nine")
+            (intValue: 1, spelled: "one"),
+            (intValue: 2, spelled: "two"),
+            (intValue: 3, spelled: "three"),
+            (intValue: 4, spelled: "four"),
+            (intValue: 5, spelled: "five"),
+            (intValue: 6, spelled: "six"),
+            (intValue: 7, spelled: "seven"),
+            (intValue: 8, spelled: "eight"),
+            (intValue: 9, spelled: "nine")
         ]
     }
 }
 
-struct NumberOccurrence {
+private struct NumberOccurrence {
     let intValue: Int
-    let first: Int
-    let last: Int
+    let position: Int
 }
