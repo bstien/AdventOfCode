5a0642e5eb855e383528c929a460c145254a494c
Bastian Stien <bastian.stien@gmail.com>
2023-12-02 11:42:35 +0100

    Day 2 – part 1

---
+++ AdventOfCode/2023/Day2.2023.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+extension Year2023.Day2: Runnable {
+    func run(input: String) {
+        let lines = splitInput(input)
+        let games = parseGames(lines: lines)
+        part1(games: games)
+    }
+
+    private func part1(games: [Game]) {
+        let possibleGames = games.filter { game in
+            game.cubeSets.allSatisfy { cubeSet in
+                cubeSet.red <= 12 &&
+                cubeSet.green <= 13 &&
+                cubeSet.blue <= 14
+            }
+        }
+
+        let gameIdSum = possibleGames.map(\.id).reduce(0, +)
+        printResult(dayPart: 1, message: "Total: \(gameIdSum)")
+    }
+
+    private func parseGames(lines: [String]) -> [Game] {
+        lines.compactMap { line in
+            let components = splitInput(line, separatedBy: ": ")
+
+            guard let gameId = Int(splitInput(components[0], separatedBy: " ")[1]) else {
+                fatalError("Could not parse gameId: \(line)")
+            }
+
+            let setComponents = splitInput(components[1], separatedBy: "; ").map {
+                splitInput($0, separatedBy: ", ").map {
+                    splitInput($0, separatedBy: " ")
+                }
+            }
+
+            let cubeSets = setComponents.map(CubeSet.create(from:))
+            return Game(id: gameId, cubeSets: cubeSets)
+        }
+    }
+}
+
+private struct Game {
+    let id: Int
+    let cubeSets: [CubeSet]
+}
+
+private class CubeSet {
+    var red: Int
+    var green: Int
+    var blue: Int
+
+    init() {
+        red = 0
+        green = 0
+        blue = 0
+    }
+
+    static func create(from components: [[String]]) -> CubeSet {
+        let cubeSet = CubeSet()
+        components.forEach { component in
+            guard component.count == 2 else {
+                fatalError("CubeSet: Did not receive 2 component: \(component)")
+            }
+
+            guard let count = Int(component[0]) else {
+                fatalError("CubeSet: Could not parse count: \(component)")
+            }
+
+            let color = component[1]
+
+            switch color {
+            case "red":
+                cubeSet.red = count
+            case "green":
+                cubeSet.green = count
+            case "blue":
+                cubeSet.blue = count
+            default:
+                fatalError("CubeSet: Could not identify color \(component)")
+            }
+        }
+        return cubeSet
+    }
+}
--- AdventOfCode/2023/Year2023.swift
+++ AdventOfCode/2023/Year2023.swift
@@ -4,9 +4,25 @@ enum Year2023: Year {
     static var year = 2023
     static var days: [Day.Type] = [
         Day1.self,
+        Day2.self,
     ]
 }
 
 extension Year2023 {
     class Day1: Day {}
+    class Day2: Day {}
+    class Day3: Day {}
+    class Day4: Day {}
+    class Day5: Day {}
+    class Day6: Day {}
+    class Day7: Day {}
+    class Day8: Day {}
+    class Day9: Day {}
+    class Day10: Day {}
+    class Day11: Day {}
+    class Day12: Day {}
+    class Day13: Day {}
+    class Day14: Day {}
+    class Day15: Day {}
+    class Day16: Day {}
 }
