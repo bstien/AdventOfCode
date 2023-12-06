60bc202b47ca63d93dd12f835161746d7f21d271
Bastian Stien <bastian.stien@gmail.com>
2023-12-06 18:20:40 +0100

    Day 6 – part 2

--- AdventOfCode/2023/Day6.2023.swift
+++ AdventOfCode/2023/Day6.2023.swift
@@ -4,20 +4,31 @@ extension Year2023.Day6: Runnable {
     func run(input: String) {
         let results = parseResults(input: input)
         part1(results: results)
+        part2(results: results)
     }
 
     private func part1(results: [RaceResult]) {
-        let totalWins = results.map { result in
-            var wins = 0
-            for ms in (1...result.time) {
-                if ms * (result.time - ms) > result.distance {
-                    wins += 1
-                }
-            }
-            return wins
+        let totalWins = results
+            .map { checkWins(time: $0.time, distance: $0.distance) }
+            .reduce(1, *)
+
+        printResult(dayPart: 1, message: "Total wins: \(totalWins)")
+    }
+
+    private func part2(results: [RaceResult]) {
+        guard
+            let time = results.map(\.time).map(String.init).joined().toInt,
+            let distance = results.map(\.distance).map(String.init).joined().toInt
+        else {
+            fatalError()
         }
 
-        printResult(dayPart: 1, message: "Total: \(totalWins.reduce(1, *))")
+        let totalWins = checkWins(time: time, distance: distance)
+        printResult(dayPart: 2, message: "Total wins: \(totalWins)")
+    }
+
+    private func checkWins(time: Int, distance: Int) -> Int {
+        (1...time).filter { $0 * (time - $0) > distance }.count
     }
 
     private func parseResults(input: String) -> [RaceResult] {
