e4042e6d0d1235a940bfaf9842b6ae5376033bcb
Bastian Stien <bastian.stien@gmail.com>
2023-12-07 19:09:04 +0100

    Day 7 – part 2

--- AdventOfCode/2023/Day7.2023.swift
+++ AdventOfCode/2023/Day7.2023.swift
@@ -4,12 +4,13 @@ extension Year2023.Day7: Runnable {
     func run(input: String) {
         let hands = parseHands(input: input)
         part1(hands: hands)
+        part2(hands: hands)
     }
 
     private func part1(hands: [Hand]) {
         let sorted = hands.sorted(by: {
-            if $0.kindValue > $1.kindValue { return false }
-            if $0.kindValue < $1.kindValue { return true }
+            if $0.kind.rawValue > $1.kind.rawValue { return false }
+            if $0.kind.rawValue < $1.kind.rawValue { return true }
 
             for pair in zip($0.hand, $1.hand) {
                 if pair.0 == pair.1 { continue }
@@ -26,11 +27,33 @@ extension Year2023.Day7: Runnable {
         printResult(dayPart: 1, message: "Winnings: \(winnings)")
     }
 
+    private func part2(hands: [Hand]) {
+        let sorted = hands.sorted(by: {
+            if $0.improvedKind.rawValue > $1.improvedKind.rawValue { return false }
+            if $0.improvedKind.rawValue < $1.improvedKind.rawValue { return true }
+
+            for pair in zip($0.hand, $1.hand) {
+                if pair.0 == pair.1 { continue }
+                return pair.0.partTwoValue < pair.1.partTwoValue
+            }
+
+            return true
+        })
+
+        let winnings = sorted.enumerated().reduce(0, { sum, tuple in
+            sum + (tuple.element.bet * (tuple.offset + 1))
+        })
+
+        printResult(dayPart: 2, message: "Winnings: \(winnings)")
+    }
+
     private func parseHands(input: String) -> [Hand] {
         splitInput(input).map {
             let components = splitInput($0, separatedBy: " ")
-
-            guard let bet = components[1].toInt else { fatalError("Could not parse Int: '\(components[1])'") }
+            
+            guard let bet = components[1].toInt else {
+                fatalError("Could not parse Int: '\(components[1])'")
+            }
 
             return Hand(hand: components[0], bet: bet)
         }
@@ -38,17 +61,15 @@ extension Year2023.Day7: Runnable {
 
     private struct Hand {
         let kind: HandKind
+        let improvedKind: HandKind
         let hand: [Card]
         let bet: Int
-
-        var kindValue: Int {
-            kind.rawValue
-        }
 
         init(hand: String, bet: Int) {
             self.kind = Self.parseHandKind(hand: hand)
             self.hand = hand.compactMap(Card.init(character:))
             self.bet = bet
+            self.improvedKind = Self.improveWithJokers(hand: self.hand, currentKind: self.kind)
         }
 
         private static func parseHandKind(hand: String) -> HandKind {
@@ -64,6 +85,32 @@ extension Year2023.Day7: Runnable {
 
             return values.filter { $0 == 2 }.count == 2 ? .twoPair : .onePair
         }
+
+        private static func improveWithJokers(hand: [Card], currentKind: HandKind) -> HandKind {
+            let grouped = hand.grouped(by: { $0 }).mapValues(\.count)
+
+            let jokerCount = grouped[.jack, default: 0]
+
+            if jokerCount == 0 { return currentKind }
+
+            switch currentKind {
+            case .highCard:
+                return .onePair
+            case .onePair:
+                return .threeOfAKind
+            case .twoPair:
+                if jokerCount == 1 { return .fullHouse }
+                return .fourOfAKind
+            case .threeOfAKind:
+                return .fourOfAKind
+            case .fullHouse:
+                return .fiveOfAKind
+            case .fourOfAKind:
+                return .fiveOfAKind
+            case .fiveOfAKind:
+                return .fiveOfAKind
+            }
+        }
     }
 
     private enum HandKind: Int {
@@ -77,7 +124,7 @@ extension Year2023.Day7: Runnable {
     }
 
     private enum Card: Int {
-        case two
+        case two = 2
         case three
         case four
         case five
@@ -90,6 +137,11 @@ extension Year2023.Day7: Runnable {
         case queen
         case king
         case ace
+
+        var partTwoValue: Int {
+            if self == .jack { return 1 }
+            return rawValue
+        }
 
         init(character: Character) {
             switch character {
