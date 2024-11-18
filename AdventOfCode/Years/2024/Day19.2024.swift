114e0bb520857360622f3d5cd28d856db8eb3b3c
Bastian Stien <bastian.stien@gmail.com>
2024-11-18 21:36:02 +0100

    Add files for 2024

---
+++ AdventOfCode/Years/2024/Day19.2024.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension Year2024.Day19: Runnable {
+    func run(input: String) {
+        let lines = splitInput(input)
+        part1(lines: lines)
+        part2(lines: lines)
+    }
+
+    private func part1(lines: [String]) {
+    }
+
+    private func part2(lines: [String]) {
+    }
+}
