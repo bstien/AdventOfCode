d8b9d18beb724de02fe641fd0b69bbfcff9585b5
Bastian Stien <bastian.stien@gmail.com>
2024-12-01 13:46:28 +0100

    Day 1 – part 2

--- AdventOfCode/Years/2024/Day1.2024.swift
+++ AdventOfCode/Years/2024/Day1.2024.swift
@@ -4,6 +4,7 @@ extension Year2024.Day1: Runnable {
     func run(input: String) {
         let values = splitInput(input).map { splitInput($0, separatedBy: " ").map { Int($0) ?? 0 } }
         part1(values)
+        part2(values)
     }
 
     private func part1(_ values: [[Int]]) {
@@ -18,5 +19,17 @@ extension Year2024.Day1: Runnable {
     }
 
     private func part2(_ values: [[Int]]) {
+        let leftValues = values.map { $0[0] }
+        let rightValues = values.map { $0[1] }
+
+        let rightGrouped = rightValues.reduce(into: [Int: Int]()) { dict, value in
+            dict[value, default: 0] += 1
+        }
+
+        var similarityScore = 0
+        for value in leftValues {
+            similarityScore += value * rightGrouped[value, default: 0]
+        }
+        printResult(dayPart: 2, message: "Similarity score: \(similarityScore)")
     }
 }
