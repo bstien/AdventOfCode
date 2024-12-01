2ff0957decb154538ee5532373667ebe28d6f2e4
Bastian Stien <bastian.stien@gmail.com>
2024-12-01 13:40:46 +0100

    Day 1 – part 1

--- AdventOfCode/Years/2024/Day1.2024.swift
+++ AdventOfCode/Years/2024/Day1.2024.swift
@@ -2,14 +2,21 @@ import Foundation
 
 extension Year2024.Day1: Runnable {
     func run(input: String) {
-        let lines = splitInput(input)
-        part1(lines: lines)
-        part2(lines: lines)
+        let values = splitInput(input).map { splitInput($0, separatedBy: " ").map { Int($0) ?? 0 } }
+        part1(values)
     }
 
-    private func part1(lines: [String]) {
+    private func part1(_ values: [[Int]]) {
+        let leftValues = values.map { $0[0] }.sorted()
+        let rightValues = values.map { $0[1] }.sorted()
+
+        var totalDiff = 0
+        for value in zip(leftValues, rightValues) {
+            totalDiff += abs(value.0 - value.1)
+        }
+        printResult(dayPart: 1, message: "Total diff: \(totalDiff)")
     }
 
-    private func part2(lines: [String]) {
+    private func part2(_ values: [[Int]]) {
     }
 }
