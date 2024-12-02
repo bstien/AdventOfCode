bc63581622ae4699869d57a702a5c77f152f8acd
Bastian Stien <bastian.stien@gmail.com>
2024-12-02 18:12:33 +0100

    Day 2 – part 2

--- AdventOfCode/Years/2024/Day2.2024.swift
+++ AdventOfCode/Years/2024/Day2.2024.swift
@@ -4,11 +4,31 @@ extension Year2024.Day2: Runnable {
     func run(input: String) {
         let reports = splitInput(input).map { splitInput($0, separatedBy: " ").map { Int($0) ?? 0 } }
         part1(reports: reports)
+        part2(reports: reports)
     }
 
     private func part1(reports: [[Int]]) {
-        let count = reports.filter { isSafe(level: $0, range: 1...3) }.count
-        printResult(dayPart: 1, message: "# of safe levels: \(count)")
+        let safeReports = reports.filter { isSafe(level: $0, range: 1...3) }.count
+        printResult(dayPart: 1, message: "# of safe reports: \(safeReports)")
+    }
+
+    private func part2(reports: [[Int]]) {
+        let range = 1...3
+        let safeReports = reports.filter { level in
+            if isSafe(level: level, range: range) { return true }
+
+            // Report is not safe. Try to remove one level at a time.
+            // If any of them succeed the report is safe.
+            for index in level.indices {
+                var level = level
+                level.remove(at: index)
+                if isSafe(level: level, range: range) {
+                    return true
+                }
+            }
+            return false
+        }.count
+        printResult(dayPart: 2, message: "# of safe levels: \(safeReports)")
     }
 
     private func isSafe(level: [Int], range: ClosedRange<Int>, index: Int = 0, direction: Direction? = nil) -> Bool {
