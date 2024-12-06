140b4695b80d6f08093af01669b77c8930bef164
Bastian Stien <bastian.stien@gmail.com>
2024-12-06 15:43:44 +0100

    Day 6 – part 2

--- AdventOfCode/Years/2024/Day6.2024.swift
+++ AdventOfCode/Years/2024/Day6.2024.swift
@@ -4,22 +4,76 @@ extension Year2024.Day6: Runnable {
     func run(input: String) {
         let grid = splitInput(input).map { Array($0) }
         let startPosition = grid.findStart()
-        let visitedPositions = traverse(grid: grid, startPosition: startPosition)
+        let visitedPositions = traverse(grid: grid, startPosition: startPosition, startDirection: .north)
 
         part1(visitedPositions: visitedPositions)
+        part2(grid: grid, startPosition: startPosition, visitedPositions: visitedPositions)
     }
 
     private func part1(visitedPositions: Set<Position>) {
         printResult(dayPart: 1, message: "Visited positions: \(visitedPositions.count)")
     }
 
+    private func part2(grid: [[Character]], startPosition: Position, visitedPositions: Set<Position>) {
+        var obstaclePositions = Set<Position>()
+        var visitedPositions = visitedPositions
+        visitedPositions.remove(startPosition)
+
+        for potentialObstaclePosition in visitedPositions {
+            var modifiedGrid = grid
+            modifiedGrid[potentialObstaclePosition.y][potentialObstaclePosition.x] = "#"
+            var visitedObstacles = Set<VisitedObstacle>()
+
+            traverse(
+                grid: modifiedGrid,
+                startPosition: startPosition,
+                startDirection: .north,
+                didHitObstacle: { obstaclePosition, direction in
+                    let obstacle = VisitedObstacle(obstaclePosition, direction)
+
+                    if visitedObstacles.contains(obstacle) {
+                        obstaclePositions.insert(potentialObstaclePosition)
+                        return true
+                    } else {
+                        visitedObstacles.insert(obstacle)
+                        return false
+                    }
+                },
+                didVisit: { _, _ in return false }
+            )
+        }
+
+        printResult(dayPart: 2, message: "Number of obstacles: \(obstaclePositions.count)")
+    }
+
     private func traverse(
         grid: [[Character]],
-        startPosition: Position
+        startPosition: Position,
+        startDirection: Direction
     ) -> Set<Position> {
         var visitedPositions: Set<Position> = [startPosition]
+        traverse(
+            grid: grid,
+            startPosition: startPosition,
+            startDirection: startDirection,
+            didVisit: { position, _ in
+                visitedPositions.insert(position)
+                return false
+            }
+        )
+
+        return visitedPositions
+    }
+
+    private func traverse(
+        grid: [[Character]],
+        startPosition: Position,
+        startDirection: Direction,
+        didHitObstacle: (Position, Direction) -> Bool = { _, _ in false },
+        didVisit: (Position, Direction) -> Bool = { _, _ in false }
+    ) {
         var position = startPosition
-        var direction = Direction.north
+        var direction = startDirection
         var isInBounds = true
 
         repeat {
@@ -27,19 +81,22 @@ extension Year2024.Day6: Runnable {
             isInBounds = grid.isInBounds(nextPosition)
             if isInBounds {
                 if grid[nextPosition.y][nextPosition.x] == "#" {
+                    if didHitObstacle(nextPosition, direction) {
+                        break
+                    }
                     direction.turnRight()
                 } else {
                     position = nextPosition
-                    visitedPositions.insert(position)
+                    if didVisit(position, direction) {
+                        break
+                    }
                 }
             }
         } while isInBounds
-
-        return visitedPositions
     }
 }
 
-private enum Direction: CaseIterable {
+private enum Direction: CaseIterable, Hashable {
     case north
     case east
     case west
@@ -64,6 +121,16 @@ private enum Direction: CaseIterable {
     }
 }
 
+private struct VisitedObstacle: Hashable {
+    let position: Position
+    let direction: Direction
+
+    init(_ position: Position, _ direction: Direction) {
+        self.position = position
+        self.direction = direction
+    }
+}
+
 private struct Vector: Hashable {
     let y: Int
     let x: Int
@@ -80,6 +147,14 @@ private struct Position: Hashable {
     static func +(lhs: Position, rhs: Direction) -> Position {
         lhs + rhs.vector
     }
+
+    static func -(lhs: Position, rhs: Vector) -> Position {
+        Position(y: lhs.y - rhs.y, x: lhs.x - rhs.x)
+    }
+
+    static func -(lhs: Position, rhs: Direction) -> Position {
+        lhs - rhs.vector
+    }
 }
 
 private extension [[Character]] {
@@ -94,5 +169,11 @@ private extension [[Character]] {
 
     func isInBounds(_ position: Position) -> Bool {
         self.indices.contains(position.y) && self[position.y].indices.contains(position.x)
+    }
+}
+
+private extension [Character] {
+    func positions(of character: Character, yPos: Int) -> [Position] {
+        enumerated().compactMap { $0.element == character ? Position(y: yPos, x: $0.offset) : nil }
     }
 }
