cd927470964f4f9cebcdb44fbb13c9b8893480d7
Bastian Stien <bastian.stien@gmail.com>
2024-12-08 12:46:11 +0100

    Day 7 – part 2

--- AdventOfCode/Years/2024/Day7.2024.swift
+++ AdventOfCode/Years/2024/Day7.2024.swift
@@ -4,30 +4,43 @@ extension Year2024.Day7: Runnable {
     func run(input: String) {
         let calibrations = parseCalibrations(input)
         part1(calibrations: calibrations)
+        part2(calibrations: calibrations)
     }
 
     private func part1(calibrations: [Int: [Int]]) {
-        var validSum = 0
-        for calibration in calibrations {
-            let isValid = check(values: calibration.value, testValue: calibration.key)
-            if isValid {
-                validSum += calibration.key
-            }
-        }
-
-        printResult(dayPart: 1, message: "Valid calibrations: \(validSum)")
+        let sum = checkCalibrations(calibrations, useCombine: false)
+        printResult(dayPart: 1, message: "Valid calibrations: \(sum)")
     }
 
-    private func check(index: Int = 0, values: [Int], testValue: Int, sum: Int = 0) -> Bool {
+    private func part2(calibrations: [Int: [Int]]) {
+        let sum = checkCalibrations(calibrations, useCombine: true)
+        printResult(dayPart: 2, message: "Valid calibrations: \(sum)")
+    }
+
+    private func checkCalibrations(_ calibrations: [Int: [Int]], useCombine: Bool) -> Int {
+        calibrations
+            .filter { check(values: $0.value, testValue: $0.key, useCombine: useCombine) }
+            .reduce(0) { $0 + $1.key }
+    }
+
+    private func check(
+        index: Int = 0,
+        values: [Int],
+        testValue: Int,
+        sum: Int = 0,
+        useCombine: Bool
+    ) -> Bool {
         guard values.indices.contains(index) else { return sum == testValue }
         guard sum <= testValue else { return false }
 
         let value = values[index]
         let additionSum = sum + value
         let multiplicationSum = max(sum, 1) * value
+        let joinSum = Int("\(sum)\(value)")!
 
-        return check(index: index + 1, values: values, testValue: testValue, sum: additionSum) ||
-        check(index: index + 1, values: values, testValue: testValue, sum: multiplicationSum)
+        return check(index: index + 1, values: values, testValue: testValue, sum: additionSum, useCombine: useCombine) ||
+        check(index: index + 1, values: values, testValue: testValue, sum: multiplicationSum, useCombine: useCombine) ||
+        (useCombine ? check(index: index + 1, values: values, testValue: testValue, sum: joinSum, useCombine: useCombine) : false)
     }
 
     private func parseCalibrations(_ input: String) -> [Int: [Int]] {
