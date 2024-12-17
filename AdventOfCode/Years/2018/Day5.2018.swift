c86727b0c8d1e36ecf519fd77eb2af0b3b34e92c
Bastian Stien <bastian.stien@gmail.com>
2024-12-17 19:16:04 +0100

    Day 5 – part 1 (2018)

--- AdventOfCode/Years/2018/Day5.2018.swift
+++ AdventOfCode/Years/2018/Day5.2018.swift
@@ -2,10 +2,39 @@ import Foundation
 
 extension Year2018.Day5: Runnable {
     func run(input: String) {
-        let lines = splitInput(input)
-        part1(lines: lines)
+        let polymer = splitInput(input).flatMap(Array.init)
+        part1(polymer: polymer)
     }
 
-    private func part1(lines: [String]) {
+    private func part1(polymer: [Character]) {
+        var polymer = polymer
+        var madeChange = false
+
+        repeat {
+            madeChange = false
+            for index in (0..<polymer.count - 1) {
+                guard let nextChar = polymer.getNext(index) else { break }
+                if polymer[index].willReact(nextChar) {
+                    polymer.remove(at: index + 1)
+                    polymer.remove(at: index)
+                    madeChange = true
+                }
+            }
+        } while madeChange == true
+
+        printResult(dayPart: 1, message: "Polymer lenght: \(polymer.count)")
+    }
+}
+
+private extension Character {
+    func willReact(_ other: Character) -> Bool {
+        self.lowercased() == other.lowercased() &&
+        self.isLowercase != other.isLowercase
+    }
+}
+
+private extension [Character] {
+    func getNext(_ index: Int) -> Character? {
+        indices.contains(index + 1) ? self[index + 1] : nil
     }
 }
