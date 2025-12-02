f7ed55d890b24e77e150dba8c820e83111026886
Bastian Stien <bastian.stien@gmail.com>
2025-12-02 13:07:57 +0100

    Day 2 – part 2

--- AdventOfCode/Years/2025/Day2.2025.swift
+++ AdventOfCode/Years/2025/Day2.2025.swift
@@ -1,10 +1,15 @@
 import Foundation
+import Algorithms
 
 extension Year2025.Day2: Runnable {
     func run(input: String) {
         let ranges = splitInput(input, separatedBy: ",").map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }.map(Range.parse)
+
         let part1Sum = part1(ranges: ranges)
         printResult(dayPart: 1, message: "Sum of ids: \(part1Sum)")
+
+        let part2Sum = part2(ranges: ranges)
+        printResult(dayPart: 2, message: "Sum of ids: \(part2Sum)")
     }
 
     private func part1(ranges: [Range]) -> Int {
@@ -22,6 +27,31 @@ extension Year2025.Day2: Runnable {
                 if firstHalf == secondHalf {
                     repeatingIds.append(id)
                 }
+            }
+        }
+
+        return repeatingIds.reduce(0, +)
+    }
+
+    private func part2(ranges: [Range]) -> Int {
+        var repeatingIds = [Int]()
+
+        for range in ranges {
+            for id in range.closedRange {
+                let s = id.description
+                var chunkSize = 0
+
+                repeat {
+                    chunkSize += 1
+                    if s.count % chunkSize != 0 { continue }
+                    let chunks = s.chunks(ofCount: chunkSize)
+                    guard chunks.count >= 2 else { continue }
+
+                    if Set(chunks).count == 1 {
+                        repeatingIds.append(id)
+                        break
+                    }
+                } while chunkSize <= (s.count / 2)
             }
         }
 
