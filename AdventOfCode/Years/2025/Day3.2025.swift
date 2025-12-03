90de22c472e13783568747253b67f93e94e06cd3
Bastian Stien <bastian.stien@gmail.com>
2025-12-03 17:28:28 +0100

    Day 3 – part 1

--- AdventOfCode/Years/2025/Day3.2025.swift
+++ AdventOfCode/Years/2025/Day3.2025.swift
@@ -2,10 +2,29 @@ import Foundation
 
 extension Year2025.Day3: Runnable {
     func run(input: String) {
-        let lines = splitInput(input)
-        part1(lines: lines)
+        let banks = splitInput(input).map { Array($0).map(String.init).compactMap(Int.init) }
+
+        let part1Sum = part1(banks: banks)
+        printResult(dayPart: 1, message: "Sum of batteries: \(part1Sum)")
     }
 
-    private func part1(lines: [String]) {
+    private func part1(banks: [[Int]]) -> Int {
+        banks.reduce(0) { $0 + findHighestCombination(in: $1) }
+    }
+
+    private func findHighestCombination(in batteries: [Int]) -> Int {
+        var high = 0
+        var low = 0
+
+        for (index, battery) in batteries.enumerated() {
+            if battery > high && index != batteries.count - 1 {
+                high = battery
+                low = 0
+            } else if battery > low {
+                low = battery
+            }
+        }
+
+        return high * 10 + low
     }
 }
