40c8c5af4f4f0550d967e1b9973bcdc7a1baeed1
Bastian Stien <bastian.stien@gmail.com>
2025-12-04 09:02:34 +0100

    Day 3 – part 2

--- AdventOfCode/Years/2025/Day3.2025.swift
+++ AdventOfCode/Years/2025/Day3.2025.swift
@@ -6,25 +6,49 @@ extension Year2025.Day3: Runnable {
 
         let part1Sum = part1(banks: banks)
         printResult(dayPart: 1, message: "Sum of batteries: \(part1Sum)")
+
+        let part2Sum = part2(banks: banks)
+        printResult(dayPart: 2, message: "Sum of batteries: \(part2Sum)")
     }
 
     private func part1(banks: [[Int]]) -> Int {
-        banks.reduce(0) { $0 + findHighestCombination(in: $1) }
+        banks.reduce(0) { $0 + findHighestCombination(in: $1, keepHighest: 2) }
     }
 
-    private func findHighestCombination(in batteries: [Int]) -> Int {
-        var high = 0
-        var low = 0
+    private func part2(banks: [[Int]]) -> Int {
+        banks.reduce(0) { $0 + findHighestCombination(in: $1, keepHighest: 12) }
+    }
+
+    private func findHighestCombination(in batteries: [Int], keepHighest keepCount: Int) -> Int {
+        var toKeep = Array(repeating: 0, count: keepCount)
+        var nextIndexToInsert = 0
 
         for (index, battery) in batteries.enumerated() {
-            if battery > high && index != batteries.count - 1 {
-                high = battery
-                low = 0
-            } else if battery > low {
-                low = battery
+            let minIndexToCheck = max(0, index - abs(keepCount - batteries.count))
+
+            var hasInserted = false
+            for (i, b) in toKeep.enumerated() {
+                if i < minIndexToCheck {
+                    continue
+                }
+
+                if hasInserted {
+                    toKeep[i] = 0
+                } else if battery > b {
+                    toKeep[i] = battery
+                    nextIndexToInsert = i
+                    hasInserted = true
+                }
             }
+            nextIndexToInsert += 1
         }
 
-        return high * 10 + low
+
+        let sum = toKeep
+            .reversed()
+            .enumerated()
+            .reduce(0) { $0 + ($1.element * max(1, Int(pow(10, Double($1.offset))))) }
+
+        return sum
     }
 }
