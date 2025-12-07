391ff4bfcb25ddb6ce5c3bb7a42c01f162570381
Bastian Stien <bastian.stien@gmail.com>
2025-12-07 10:34:53 +0100

    Day 7 – part 2 (naive solution)

--- AdventOfCode/Years/2025/Day7.2025.swift
+++ AdventOfCode/Years/2025/Day7.2025.swift
@@ -9,26 +9,29 @@ extension Year2025.Day7: Runnable {
         }
 
         let splitters = lines.map { line in
-            line.enumerated().compactMap { index, char in
+            let array = line.enumerated().compactMap { index, char in
                 if char == "^" {
                     return index
                 }
                 return nil
             }
+
+            return Set(array)
         }
 
         let part1Sum = part1(start: start, splitters: splitters)
         printResult(dayPart: 1, message: "# of splits: \(part1Sum)")
+
+        let part2Sum = part2(column: start, row: 1, splitters: splitters)
+        printResult(dayPart: 2, message: "# of splits: \(part2Sum)")
     }
 
-    private func part1(start: Int, splitters: [[Int]]) -> Int {
+    private func part1(start: Int, splitters: [Set<Int>]) -> Int {
         var numberOfSplits = 0
         var beams: Set = [start]
 
         for line in splitters {
-            let set = Set(line)
-
-            let intersection = beams.intersection(set)
+            let intersection = beams.intersection(line)
 
             numberOfSplits += intersection.count
 
@@ -43,5 +46,16 @@ extension Year2025.Day7: Runnable {
         }
 
         return numberOfSplits
+    }
+
+    private func part2(column: Int, row: Int, splitters: [Set<Int>]) -> Int {
+        if row == splitters.count - 1 { return 1 }
+
+        if splitters[row].contains(column) {
+            return part2(column: column - 1, row: row + 1, splitters: splitters) +
+            part2(column: column + 1, row: row + 1, splitters: splitters)
+        }
+
+        return part2(column: column, row: row + 1, splitters: splitters)
     }
 }
