4c811284fe25da6c57ad09ff4055f66e50bb200a
Bastian Stien <bastian.stien@gmail.com>
2025-12-08 16:52:31 +0100

    Day 8 – part 2

--- AdventOfCode/Years/2025/Day8.2025.swift
+++ AdventOfCode/Years/2025/Day8.2025.swift
@@ -3,29 +3,19 @@ import Foundation
 extension Year2025.Day8: Runnable {
     func run(input: String, isTest: Bool) {
         let coordinates = splitInput(input).map(Coordinate.parse(_:))
+        let distances = findDistances(coordinates).sorted(by: { $0.distance < $1.distance })
 
-        let part1Sum = part1(coordinates: coordinates, takeHighest: isTest ? 10 : 1000)
+        let part1Sum = part1(distances: Array(distances.prefix(isTest ? 10 : 1000)))
         printResult(dayPart: 1, message: "Sum: \(part1Sum)")
+
+        let part2Sum = part2(distances: distances, coordinates: coordinates)
+        printResult(dayPart: 2, message: "Sum: \(part2Sum)")
     }
 
-    private func part1(coordinates: [Coordinate], takeHighest: Int) -> Int {
-        var distances = [Distance]()
-        var calculatedCoordinates = Set<Coordinate>()
-
-        for coordinate in coordinates {
-            for otherCoordinate in coordinates {
-                if coordinate == otherCoordinate || calculatedCoordinates.contains(otherCoordinate) { continue }
-                let distance = Distance(a: coordinate, b: otherCoordinate, distance: coordinate.distance(to: otherCoordinate))
-                distances.append(distance)
-            }
-
-            calculatedCoordinates.insert(coordinate)
-        }
-
+    private func part1(distances: [Distance]) -> Int {
         var circuits = [Set<Coordinate>]()
-        let shortestDistances = distances.sorted(by: { $0.distance < $1.distance }).prefix(takeHighest)
 
-        for distance in shortestDistances {
+        for distance in distances {
             let aIndex = circuits.firstCircuitIndex(with: distance.a)
             let bIndex = circuits.firstCircuitIndex(with: distance.b)
 
@@ -44,6 +34,53 @@ extension Year2025.Day8: Runnable {
         }
 
         return circuits.map(\.count).sorted().suffix(3).reduce(1, *)
+    }
+
+    private func part2(distances: [Distance], coordinates: [Coordinate]) -> Int {
+        var circuits = [Set<Coordinate>]()
+
+        for distance in distances {
+            let aIndex = circuits.firstCircuitIndex(with: distance.a)
+            let bIndex = circuits.firstCircuitIndex(with: distance.b)
+
+            switch (aIndex, bIndex) {
+            case (let aIndex?, let bIndex?):
+                if aIndex == bIndex { continue }
+                circuits[aIndex].formUnion(circuits[bIndex])
+                circuits.remove(at: bIndex)
+            case (let aIndex?, .none):
+                circuits[aIndex].insert(distance.b)
+            case (.none, let bIndex?):
+                circuits[bIndex].insert(distance.a)
+            case (.none, .none):
+                circuits.append([distance.a, distance.b])
+            }
+
+            if circuits.count == 1, circuits[0].count == coordinates.count {
+                return distance.a.x * distance.b.x
+            }
+        }
+
+        fatalError("Could not find the magic circuit")
+    }
+
+    // MARK: - Helpers
+
+    private func findDistances(_ coordinates: [Coordinate]) -> [Distance] {
+        var distances = [Distance]()
+        var calculatedCoordinates = Set<Coordinate>()
+
+        for coordinate in coordinates {
+            for otherCoordinate in coordinates {
+                if coordinate == otherCoordinate || calculatedCoordinates.contains(otherCoordinate) { continue }
+                let distance = Distance(a: coordinate, b: otherCoordinate, distance: coordinate.distance(to: otherCoordinate))
+                distances.append(distance)
+            }
+
+            calculatedCoordinates.insert(coordinate)
+        }
+
+        return distances
     }
 }
 
